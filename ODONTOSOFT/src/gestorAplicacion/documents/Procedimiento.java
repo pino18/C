49d1a88922ab5be4fcb38c0b52db6ea1c7a99623
selopez2704 <51925867+selopez2704@users.noreply.github.com>
2019-07-18 20:09:34 -0500

    Update Procedimiento.java

--- ODONTOSOFT/src/gestorAplicacion/documents/Procedimiento.java
+++ ODONTOSOFT/src/gestorAplicacion/documents/Procedimiento.java
@@ -6,7 +6,36 @@ public class Procedimiento {
 	private int valor;
 	
 	public String getTipo() {
+		return Tipo;package gestorAplicacion.documents;
+
+import BaseDatos.Data;
+
+public class Procedimiento {
+	
+	private String Tipo;
+	private int valor;
+	
+	public Procedimiento(String t,int v) {
+		Tipo=t;
+		valor=v;
+		Data.proced.put(t, this);
+	}
+	
+	public String getTipo() {
 		return Tipo;
+	}
+	public void setTipo(String tipo) {
+		Tipo = tipo;
+	}
+	public int getValor() {
+		return valor;
+	}
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	// Fin Setters and Getters
+}
+
 	}
 	public void setTipo(String tipo) {
 		Tipo = tipo;
