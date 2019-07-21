cc95e3241b17105a862e2d30e10a04a10e9a10ca
selopez2704 <51925867+selopez2704@users.noreply.github.com>
2019-07-20 23:54:13 -0500

    Add files via upload

---
+++ ODONTOSOFT-master/ODONTOSOFT-master/ODONTOSOFT/src/uiMain/operations/RetirarEps.java
@@ -0,0 +1,22 @@
+package uiMain.operations;
+
+import uiMain.OpcionDeMenu;
+
+public class RetirarEps extends OpcionDeMenu {
+	public RetirarEps(String key) {
+		super(key);
+	}
+
+	@Override
+	public void ejecutar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
