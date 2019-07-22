34c73696c151af041c38586d32b83ebfec2354a9
selopez2704 <51925867+selopez2704@users.noreply.github.com>
2019-07-22 15:11:23 -0500

    Update HistoriaClinica.java

--- ODONTOSOFT-master/ODONTOSOFT-master/ODONTOSOFT/src/gestorAplicacion/documents/HistoriaClinica.java
+++ ODONTOSOFT-master/ODONTOSOFT-master/ODONTOSOFT/src/gestorAplicacion/documents/HistoriaClinica.java
@@ -26,7 +26,9 @@ public class HistoriaClinica {
 		Data.historias.put(Id,this);
 	}
 	
-
+	public void AddNroCitasRealizadas() {
+		this.NroCitasRealizadas = this.NroCitasRealizadas+1;
+	}
 	public String getID() {
 		return ID;
 	}
