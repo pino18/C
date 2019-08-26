ae96e8afe4d54f76a4827f11e3b93fe550cbe009
selopez2704 <51925867+selopez2704@users.noreply.github.com>
2019-08-25 23:27:09 -0500

    Cambie los text y area field y el oyente de enviar
    los volvi estaticos y publicos y le puse nuevo oyente a enviar

--- ODONTOSOFT-master/src/View/PanelValoracion.java
+++ ODONTOSOFT-master/src/View/PanelValoracion.java
@@ -3,37 +3,38 @@ package View;
 import java.awt.*;
 import javax.swing.*;
 import Control.ControlOption;
-
+import Control.ControlValoracion;
 public class PanelValoracion extends JPanel {
 
 	JButton volver;
 	JButton enviar;
-
+	public static JTextArea COM;
+	public static JTextField VAL;
 	public PanelValoracion() {
 		JPanel PP = new JPanel();
 		PP.setLayout(new BorderLayout(0, 7));
 		JPanel P1 = new JPanel();
 		P1.add(new JLabel("Valoración"));
 		JPanel P2 = new JPanel();
-		P2.add(new JLabel(
-				"Permite dar una valoracion al servicio prestado por la entidad al usiario de manera anonima"));
-
+		P2.add(new JLabel("Permite dar una valoracion al servicio prestado por la entidad al usiario de manera anonima"));
 		JPanel P3 = new JPanel();
 		P3.setLayout(new BorderLayout(0, 7));
 		JPanel P4 = new JPanel();
 		P4.setLayout(new GridLayout(4, 1, 0, 5));
 		P4.add(new JLabel("Por favor ingrese una valoración del 1 al 5 utiliando el punto para denotar decimales"));
-		P4.add(new JTextField(""));
+		VAL = new JTextField();
+		P4.add(VAL);
 		P4.add(new JLabel("Por favor ingrese un comentario"));
-		P4.add(new JTextArea());
+		COM = new JTextArea();
+		P4.add(COM);
 		P3.add(P4, BorderLayout.CENTER);
 		JPanel P5 = new JPanel();
 		P5.setLayout(new GridLayout(1, 2, 0, 5));
 		enviar = new JButton(InterfazVista.ENVIAR);
-		enviar.addActionListener(new ControlOption());
+		enviar.addActionListener(new ControlValoracion());
 		volver = new JButton(InterfazVista.VOLVER);
 		volver.addActionListener(new ControlOption());
-		P5.add(enviar);
+		P5.add(enviar);//FALTA EL CONTROLADOR
 		P5.add(volver);
 		P3.add(P5, BorderLayout.SOUTH);
 		PP.add(P1, BorderLayout.NORTH);
