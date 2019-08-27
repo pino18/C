3fca727564c45711127db93061057b061e5d948e
Marlon Valencia <marlondvalencia@gmail.com>
2019-08-27 12:35:00 -0500

    ControlBotonsitoPanelInicio

---
+++ ODONTOSOFT-master/src/Control/ControlPanelInicio/ControlChangeButton.java
@@ -0,0 +1,36 @@
+package Control.ControlPanelInicio;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+public class ControlChangeButton implements MouseListener {
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
