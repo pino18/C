b09c22127d244dde55c9e41a71594fe42593ba7c
selopez2704 <51925867+selopez2704@users.noreply.github.com>
2019-08-27 16:35:15 -0500

    Agregue el Controlador de ver operaciones

---
+++ ODONTOSOFT-master/src/Control/ControlSeeOpt.java
@@ -0,0 +1,33 @@
+package Control;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import View.InterfazVista;
+import errorAplication.NotFoundException;
+import uiMain.operations.SeeOpt;
+import View.PanelSeeOpt;
+import View.VentanaPP;
+public class ControlSeeOpt implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String ID = PanelSeeOpt.ID.getText();
+		if(e.getActionCommand().equals(InterfazVista.ACEPTAR)) {
+			try {
+				
+				ArrayList<String> opt = SeeOpt.ejecutar(ID);
+				VentanaPP.contenedor.removeAll();
+				VentanaPP.contenedor.add(new PanelSeeOpt(opt));
+				VentanaPP.ventana.pack();
+			}catch(NotFoundException EX) {
+				JOptionPane.showMessageDialog(null, "No existe un usuario con la identificacion "+ID, "Usuario no encontrado", JOptionPane.WARNING_MESSAGE);
+			}
+		}
+
+	}
+
+}
