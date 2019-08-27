1469666b26ea91dc4ff2269636c71403669854ec
selopez2704 <51925867+selopez2704@users.noreply.github.com>
2019-08-27 16:40:10 -0500

    Agregue el metodo ejecutar(ID)
    Agregue el metodo ejecutar con sobrecarga para el controlador de SeeOpt

--- ODONTOSOFT-master/src/uiMain/operations/SeeOpt.java
+++ ODONTOSOFT-master/src/uiMain/operations/SeeOpt.java
@@ -1,25 +1,32 @@
 package uiMain.operations;
 
 import java.util.Scanner;
-
+import java.util.ArrayList;
+import errorAplication.NotFoundException;
 import gestorAplicacion.users.User;
 import uiMain.OpcionDeMenu;
 
-public class SeeOpt extends OpcionDeMenu {//Sirve para ver todas las opciones de menú que se encuentran disponibles, todas y cada una
+public class SeeOpt extends OpcionDeMenu {//Sirve para ver todas las opciones de menÃº que se encuentran disponibles, todas y cada una
 	
 	public SeeOpt(String key){
 		super(key);
 	}
 	
 	@Override
-	public void ejecutar() {//Hace print de todas y cada una de las opciones de menú con su respectivo Key
+	public void ejecutar() {//Hace print de todas y cada una de las opciones de menÃº con su respectivo Key
 		Scanner leer = new Scanner(System.in);
 		System.out.print("Ingrese el nombre de usuario: ");
 		String username = leer.next();
 		User user = User.getUserByUsername(username);
 		user.getMenu().seeOpt();
 	}
-
+	public static ArrayList<String> ejecutar(String ID) throws NotFoundException  {
+		User user = User.getUserByUsername(ID);
+		if(user!=null) {
+			return user.getMenu().seeOpt1();
+		}throw new NotFoundException();
+	}
+	
 	@Override
 	public String toString() {
 		return "ver funciones de un usuario";
