6397cd640fd7fad87100a00ecf7810a817142d06
selopez2704 <51925867+selopez2704@users.noreply.github.com>
2019-08-27 22:23:52 -0500

    Comente y arregle una distribucion

--- ODONTOSOFT-master/src/View/PanelSeeOpt.java
+++ ODONTOSOFT-master/src/View/PanelSeeOpt.java
@@ -8,12 +8,21 @@ import Control.ControlSeeOpt;
 import java.awt.*;
 import java.util.ArrayList;
 
+/*En este panel se pueden vizualizar las opciones que el usuario a ingresar tiene*/
+
 public class PanelSeeOpt extends JPanel {
 	JButton aceptar;
 	JButton volver;
 	public static JTextField ID;
 
 	public PanelSeeOpt() {
+		
+		/*Este constructor crea un panel en donde se encuentra un JTextField
+		 en donde se ingresa el nombre de usuario y
+		 tambien genera los botones aceptar y volver.
+		 
+		 Es generado por el controlador de la opcion de menu*/
+		
 		JPanel PP = new JPanel();
 		PP.setLayout(new BorderLayout());
 
@@ -25,11 +34,10 @@ public class PanelSeeOpt extends JPanel {
 		JPanel P2 = new JPanel();
 		P2.setLayout(new GridLayout(1, 2, 5, 5));
 		P2.add(new JLabel("Ingrese el nombre de usuario"));
-		ID = new JTextField();
+		ID = new JTextField("");
 		P2.add(ID);
 
 		JPanel P3 = new JPanel();
-		P3.setLayout(new GridLayout(1, 2, 5, 5));
 		aceptar = new JButton(InterfazVista.ACEPTAR);
 		aceptar.addActionListener(new ControlSeeOpt());
 		volver = new JButton(InterfazVista.CANCELAR);
@@ -43,26 +51,35 @@ public class PanelSeeOpt extends JPanel {
 	}
 
 	public PanelSeeOpt(ArrayList<String> A) {
+		
+		/*Este constructor crea un panel en donde se encuentra un conjunto de JLabels 
+		 en donde se visualiza las opciones de menu del nombre de usuario ingresado y
+		 tambien genera el boton volver.
+		 
+		 Es generado por el controlador del panel ver opciones (ControlSeeOpt)*/
 		JPanel PP = new JPanel();
+		PP.setLayout(new BorderLayout());
 		
 		JPanel P1 = new JPanel();
-		P1.setLayout(new GridLayout(3,1,5,5));
+		P1.setLayout(new GridLayout(4,1,10,10));
 		P1.add(new JLabel("Vizualizacion de las opciones de usuario"));
 		P1.add(new JLabel("Permite obtener las opciones de menu que tiene el usuario que se ingrese"));
-		P1.add(new JLabel("Las opciones de menu de usuario +ID+"son:"));
+		P1.add(new JLabel("Las opciones de menu de usuario "+ID.getText()+" son:"));
+		P1.add(new JLabel(""));
 
 		JPanel P2 = new JPanel();
-		P2.setLayout(new GridLayout(2,20,5,5));
+		P2.setLayout(new GridLayout(A.size()+1,1,5,5));
 		int I;
 		for(I=0;I<A.size();I++){
 			P2.add(new JLabel((A.get(I))));
-		}
+		}P2.add(new JLabel(""));
 		
 		JPanel P3 = new JPanel();
-		P3.setLayout(new GridLayout(1,2,5,5));
-		volver = new JButton(InterfazVista.CANCELAR);
+		volver = new JButton("Volver");
+		volver.setActionCommand(InterfazVista.CANCELAR);
 		volver.addActionListener(new ControlMenu());
 		P3.add(volver);
+		
 		PP.add(P1, BorderLayout.NORTH);
 		PP.add(P2, BorderLayout.CENTER);
 		PP.add(P3, BorderLayout.SOUTH);
