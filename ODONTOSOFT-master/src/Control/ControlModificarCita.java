82e6ef83b2e6cd9d4b0917881f911f6725ec335c
pino18 <51934567+pino18@users.noreply.github.com>
2019-08-28 02:29:07 -0500

    Comentarios final

--- ODONTOSOFT-master/src/Control/ControlModificarCita.java
+++ ODONTOSOFT-master/src/Control/ControlModificarCita.java
@@ -1,51 +1,70 @@
-package Control;
+package View;
 import java.awt.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.util.ArrayList;
-
 import javax.swing.*;
 
-import View.PanelCancelarCita;
-import View.PanelModificarCita;
-import View.VentanaPP;
-import View.FieldPanel.FieldPanel;
+import Control.ControlCancelarCita;
+import Control.ControlModificarCita;
+import Control.ControlOption;
+import Control.ControlRegistrar;
+import gestorAplicacion.documents.Procedimiento;
 
-public class ControlModificarCita implements ItemListener {
-
-	@Override
-	public void itemStateChanged(ItemEvent e) {
-		String op = (String) e.getItem();
-		JComboBox<String> option1 = new JComboBox<String>() ;
-		if(op.equals("Cambiar El Estado De Una Cita")) {
-			option1.addItem("---");
-			option1.addItem("Pendiente");
-			option1.addItem("Realizada");
-			
-			
-			PanelModificarCita.PP.remove(PanelModificarCita.Field);
-			PanelModificarCita.PP.remove(PanelModificarCita.PF);
-			PanelModificarCita.Field.removeAll();
-			PanelModificarCita.Field.add(option1);
-			PanelModificarCita.PP.add(PanelModificarCita.Field, BorderLayout.CENTER);
-			PanelModificarCita.PP.add(PanelModificarCita.PF, BorderLayout.SOUTH);
-			VentanaPP.ventana.pack();
-			
-		}
-		else if(op.equals("Ingresar El Procedimiento Realizado En La Cita")) {
-			
-			PanelModificarCita.PP.remove(PanelModificarCita.Field);
-			PanelModificarCita.PP.remove(PanelModificarCita.PF);
-			PanelModificarCita.Field.removeAll();
-			PanelModificarCita.Field.add(new JTextArea(10,30));
-			PanelModificarCita.PP.add(PanelModificarCita.Field, BorderLayout.CENTER);
-			PanelModificarCita.PP.add(PanelModificarCita.PF, BorderLayout.SOUTH);
-			VentanaPP.ventana.pack();
-			
-			
-		}
+public class PanelModificarCita extends JPanel {
+	JButton volver;
+	JButton verificar;
+	public static JComboBox<String> option;
+	public static JPanel PP;
+	public static JPanel Field = new PanelVoid();
+	public static JPanel PF;
+	public static JTextField numcita;
+	public static JTextField procedi;
+	
+	
+	public PanelModificarCita() {
+		PP = new JPanel();
+		PP.setLayout(new BorderLayout(7,7));
+		
+		JPanel title = new JPanel();
+		title.setLayout(new GridLayout(3,1,7,7));
+		title.add(new JLabel("Modificar Cita"));
+		JPanel text = new JPanel();
+		text.add(new JLabel("Digite el número de la cita a modificar: "));
+		numcita = new JTextField("",15);
+		text.add(numcita);
+		title.add(text);
+		
+		
+		
+		JPanel PM=new JPanel();
+		PM.setLayout(new BorderLayout());
+		JPanel PCENTER = new JPanel(); 
+		PCENTER.add(new JLabel("La cita fue realizada con exito: "));
+		option = new JComboBox();
+		option.addItem("---");
+		option.addItem("Si");
+		option.addItem("No");
+		PCENTER.add(option);
+		PM.add(PCENTER,BorderLayout.NORTH);
+		PM.add(new JLabel("Ingrese el Procedimiento Realizado: "), BorderLayout.CENTER);
+		procedi=new JTextField();
+		PM.add(procedi,BorderLayout.SOUTH);
+		
+		
+		PF=new JPanel();
+		verificar=new JButton(InterfazVista.ENVIAR);
+		verificar.addActionListener(new ControlModificarCita());
+		volver = new JButton(InterfazVista.VOLVER);
+		volver.addActionListener(new ControlOption());
+		PF.add(verificar);
+		PF.add(volver);
+		
+		
+		PP.add(title, BorderLayout.NORTH);
+		PP.add(PM,BorderLayout.CENTER);
+		PP.add(PF,BorderLayout.SOUTH);
+		this.add(PP);
+		
+		
 		
 	}
-	
 
 }
