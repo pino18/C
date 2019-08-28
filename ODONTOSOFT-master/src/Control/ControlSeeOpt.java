c25de4e7c6515d5fcf504a3696057575ed48c551
selopez2704 <51925867+selopez2704@users.noreply.github.com>
2019-08-27 22:21:28 -0500

    Comente el código

--- ODONTOSOFT-master/src/Control/ControlSeeOpt.java
+++ ODONTOSOFT-master/src/Control/ControlSeeOpt.java
@@ -11,23 +11,33 @@ import errorAplication.NotFoundException;
 import uiMain.operations.SeeOpt;
 import View.PanelSeeOpt;
 import View.VentanaPP;
+//Este controlador premite modificar el panel de Ver opciones de usuario
+
 
 public class ControlSeeOpt implements ActionListener {
-
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String ID = PanelSeeOpt.ID.getText();
 		if (e.getActionCommand().equals(InterfazVista.ACEPTAR)) {
 			try {
 				if (!ID.equals("")) {
+					/*Este if valida que la persona llene 
+					el campo nombre de usuario del panel ver opciones*/
+					
 					ArrayList<String> opt = SeeOpt.ejecutar(ID);
 					VentanaPP.contenedor.removeAll();
 					VentanaPP.contenedor.add(new PanelSeeOpt(opt));
 					VentanaPP.ventana.pack();
 				}else {
+					/*Arroja una ventana emergente en caso de que la persona
+					 no llene el campo nombre de usuario donde da una alerta*/
 					JOptionPane.showMessageDialog(null, "El campo identificacion del usuario no fue llenado","Campo vacio", JOptionPane.WARNING_MESSAGE);
 				}
 			} catch (NotFoundException EX) {
+				/*Este catch se encarga de arrojar una ventana emergente
+				  en caso de que el programa arroje un error por haber 
+				  ingresado un nombre de usuario que no existe*/
 				JOptionPane.showMessageDialog(null, "No existe un usuario con la identificacion " + ID,
 						"Usuario no encontrado", JOptionPane.WARNING_MESSAGE);
 			}
