afb7cc9ee6c82faf5cf1f556b91a93aa3db636e8
selopez2704 <51925867+selopez2704@users.noreply.github.com>
2019-08-27 19:27:11 -0500

    Agregue la validacion de campo lleno

--- ODONTOSOFT-master/src/Control/ControlSeeOpt.java
+++ ODONTOSOFT-master/src/Control/ControlSeeOpt.java
@@ -11,20 +11,25 @@ import errorAplication.NotFoundException;
 import uiMain.operations.SeeOpt;
 import View.PanelSeeOpt;
 import View.VentanaPP;
+
 public class ControlSeeOpt implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String ID = PanelSeeOpt.ID.getText();
-		if(e.getActionCommand().equals(InterfazVista.ACEPTAR)) {
+		if (e.getActionCommand().equals(InterfazVista.ACEPTAR)) {
 			try {
-				
-				ArrayList<String> opt = SeeOpt.ejecutar(ID);
-				VentanaPP.contenedor.removeAll();
-				VentanaPP.contenedor.add(new PanelSeeOpt(opt));
-				VentanaPP.ventana.pack();
-			}catch(NotFoundException EX) {
-				JOptionPane.showMessageDialog(null, "No existe un usuario con la identificacion "+ID, "Usuario no encontrado", JOptionPane.WARNING_MESSAGE);
+				if (!ID.equals("")) {
+					ArrayList<String> opt = SeeOpt.ejecutar(ID);
+					VentanaPP.contenedor.removeAll();
+					VentanaPP.contenedor.add(new PanelSeeOpt(opt));
+					VentanaPP.ventana.pack();
+				}else {
+					JOptionPane.showMessageDialog(null, "El campo identificacion del usuario no fue llenado","Campo vacio", JOptionPane.WARNING_MESSAGE);
+				}
+			} catch (NotFoundException EX) {
+				JOptionPane.showMessageDialog(null, "No existe un usuario con la identificacion " + ID,
+						"Usuario no encontrado", JOptionPane.WARNING_MESSAGE);
 			}
 		}
 
